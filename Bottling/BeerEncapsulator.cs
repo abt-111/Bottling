b4a369bf8deb7bc754126294ab5fcc3ca2c17f85
abt-111 <anthony.temmel@gmail.com>
2024-06-18 17:50:56 +0200

    Ajout de la classe BeerEncapsulator, de ses propriétés et méthode

---
+++ Bottling/BeerEncapsulator.cs
@@ -0,0 +1,72 @@
+﻿namespace Bottling
+{
+    internal class BeerEncapsulator
+    {
+        private float _avalaibleBeerVolume;
+        private int _avalaibleBottles;
+        private int _avalaibleCapsules;
+
+        public float AvalaibleBeerVolume => _avalaibleBeerVolume;
+
+        public int AvalaibleBottles => _avalaibleBottles;
+
+        public int AvalaibleCapsules
+        {
+            get => _avalaibleCapsules;
+            set => _avalaibleCapsules = value;
+        }
+
+        public BeerEncapsulator(float avalaibleBeerVolume, int avalaibleBottles, int avalaibleCapsules)
+        {
+            _avalaibleBeerVolume = avalaibleBeerVolume;
+            _avalaibleBottles = avalaibleBottles;
+            _avalaibleCapsules = avalaibleCapsules;
+
+        }
+
+        public void AddBeer(float beer)
+        {
+            _avalaibleBeerVolume += beer;
+        }
+
+        public int ProduceEncapsulatedBeerBottles(int bottlesQuantity)
+        {
+            int beerMade = 0;
+
+            for(int i = 0; i < bottlesQuantity; i++)
+            {
+                if(_avalaibleBottles > 0)
+                {
+                    if (_avalaibleBeerVolume >= 0.33f)
+                    {
+                        if(_avalaibleCapsules > 0)
+                        {
+                            _avalaibleBottles--;
+                            _avalaibleBeerVolume -= 0.33f;
+                            _avalaibleCapsules--;
+                            beerMade++;
+                            Console.WriteLine($"Beer n°{i + 1} made! - Beer volume level : {_avalaibleBeerVolume}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You must add {bottlesQuantity - i} capsules to continue");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You must add {(bottlesQuantity - i) * 0.33f}L of beer to continue");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"You must add {bottlesQuantity - i} bottles to continue");
+                    break;
+                }
+            }
+
+            return beerMade;
+        }
+    }
+}
--- Bottling/Program.cs
+++ Bottling/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bottling;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int beerMade;
+
+        BeerEncapsulator theBeerMustBeMade = new BeerEncapsulator(8.0f, 100, 100);
+
+        beerMade = theBeerMustBeMade.ProduceEncapsulatedBeerBottles(100);
+
+        Console.WriteLine($"You made {beerMade} beers");
+
+        theBeerMustBeMade.AddBeer(24.75f);
+
+        beerMade = theBeerMustBeMade.ProduceEncapsulatedBeerBottles(100 - beerMade);
+
+        Console.WriteLine($"You made {beerMade} beers");
+    }
+}
